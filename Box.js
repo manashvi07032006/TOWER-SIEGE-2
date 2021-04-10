7ad2fe6612c28e55bc4961017c5093f0582427db
manashvi07032006 <80456520+manashvi07032006@users.noreply.github.com>
2021-04-10 14:42:52 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,27 +1,3 @@
-/*class Box {
-  constructor(x,y,width,height) {
-    var options = {
-      'restitution':0.8,
-      'friction':0.2,
-      'density':0.02
-    }
-    this.body = Bodies.rectangle(x,y,width,height, options);
-    this.width = width;
-    this.height = height;
-    World.add(world, this.body);
-    this.Visibility = 255;
-  }
-  display(){
-    var pos =this.body.position;
-    rectMode(CENTER);
-    strokeWeight(3.5);
-    stroke("black")
-    //fill(232, 206, 237);
-    rect(pos.x, pos.y, this.width, this.height);
-    console.log(this.body.speed);
-    
-  }
-}*/
 class Box extends BaseClass{
   constructor(x,y,width,height){
     super(x, y, width, height);
